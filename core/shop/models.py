fa57bc148550269d73b51518a6eddcaa84e023d7
Arbe <bektemir1102@gmail.com>
2025-08-21 15:17:39 +0600

    model

--- core/shop/models.py
+++ core/shop/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+class Brand(models.Model):
+    title = models.CharField(max_length=100)
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+
+class Storage(models.Model):
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.stock
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
@@ -16,4 +25,10 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
