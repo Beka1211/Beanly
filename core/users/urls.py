4192605075caca1c8295ae6d78f9eba544ba795a
Arbe <bektemir1102@gmail.com>
2025-08-21 11:49:41 +0600

    кастом моделька

---
+++ core/users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path('users/register/', views.UserRegisterView.as_view()),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
