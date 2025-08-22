1793101a12210dc95865d8cff2257ebad415413a
BellKD <shaihislamova92@gmail.com>
2025-08-22 18:17:52 +0200

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from rest_framework import permissions
 from django.shortcuts import redirect
+from rest_framework import permissions
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -9,23 +9,23 @@ from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework_simplejwt.views import (     # JWT
+from rest_framework_simplejwt.views import (  # JWT
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Beanly API",
-      default_version='v1',
-      description="Документация для API кофейни",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Beanly API",
+        default_version='v1',
+        description="Документация для API кофейни",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('', lambda request: redirect('swagger/')),
+    path('', lambda request: redirect('/swagger/')),
     path('admin/', admin.site.urls),
 
     path('api/', include('users.urls')),
@@ -36,14 +36,12 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Swagger
+    # Swagger / Redoc
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0),
             name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
