d08e80772285b14a9844640d7e75a6bafc927110
BellKD <shaihislamova92@gmail.com>
2025-08-23 15:06:18 +0200

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'shop',
     'orders',
     'users',
+     "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware", 
 ]
 
 SIMPLE_JWT = {
